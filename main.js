e6e95bc2afa6590cf087a9ed8e11295d1ec93faf
Praktikant <praktikant@MPA-Praktikant-2.local>
2023-07-14 13:34:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function accept () {
+    var cookie = document.getElementById("cookieBanner");
+    cookie.classList.toggle("invis");
+    var cookie2 = document.getElementById("cookieBanner2");
+    cookie2.classList.toggle("invis");
+}
+function deny () {
+    var nocookie = document.getElementById("cookieBanner2");
+    nocookie.classList.toggle("vis")
+    var cookie = document.getElementById("cookieBanner")
+    cookie.classList.toggle("invis")
+}
+function deny2 () {
+    var cookie = document.getElementById("cookieBanner");
+    cookie.classList.toggle("invis");
+    var cookie2 = document.getElementById("cookieBanner2");
+    cookie2.classList.toggle("vis");
+    alert('WIESOOOOOOO')
+}
